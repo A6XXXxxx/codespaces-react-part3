9ca2390f3a19f3c6183ff227451b5e7b04b81e23
A6XXXxxx <91040742+A6XXXxxx@users.noreply.github.com>
2024-10-03 08:44:56 +0000

    v3

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,25 @@
 import './App.css';
+import Posts from './Posts';
+import About from './About';
+import Home from './Home';
+import { BrowserRouter , Route , Routes , Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    <BrowserRouter>
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/">Home</Link>Doungkamol</li>
+        <li><Link to="/about">About</Link></li>
+      </ul>
     </div>
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/about' element={<About/>}></Route>
+      <Route path='/posts' element={<Posts/>}></Route>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
